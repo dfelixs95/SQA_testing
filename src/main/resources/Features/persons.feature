c9ebf04fb3451f3f5ef631eb4c5604aaa5511972
Daniel Felix Setiawan <daniel.felix@ti.ukdw.ac.id>
2018-04-10 17:56:39 +0700

    Menambahkan scenario Add Phone

--- src/main/resources/Features/persons.feature
+++ src/main/resources/Features/persons.feature
@@ -3,3 +3,8 @@ Feature: CRUD Person dan Phone
         Given Ambil jumlah record person saat ini
         When Ditambahkan record baru dengan first name: Roni, last name: Purwanto, age: 45 years, phone number: 2054913867, no regis: 88888888888
 		Then Jumlah record person menjadi jumlah record awal + 1
+		
+	Scenario: Penambahan Nomor Telepon
+		Given Ambil jumlah nomor telepon milik person dengan id 5 saat ini
+		When Tambahkan nomor telepon 98785462627 untuk person dengan id 5
+		Then Jumlah nomor telepon milik person dengan id 5 menjadi jumlah awal + 1
--- src/test/java/com/example/testing/People.java
+++ src/test/java/com/example/testing/People.java
@@ -54,4 +54,38 @@ public class People {
 		JSONArray arrPerson = jsonResponse.getBody().getArray();
 		assertThat(arrPerson.length(), equalTo(jmlRecordPersonAwal+jmlPenambahanRecord));
 	}
+	
+	@Given("^Ambil jumlah nomor telepon milik person dengan id (\\d+) saat ini$")
+	public void givenAddPhone(int personId) throws Throwable{
+		HttpResponse<JsonNode> jsonResponse = Unirest.get("http://localhost:8080/persons/"+personId)
+				.header("Accept", "application/json")
+				.asJson();
+		JSONObject objPerson  = jsonResponse.getBody().getObject();
+		JSONArray arrPhoneNumber = objPerson.getJSONArray("phones");
+		jmlNoTelpAwal = arrPhoneNumber.length();
+	}
+	
+	@When("^Tambahkan nomor telepon (\\d+) untuk person dengan id (\\d+)$")
+	public void whenAddPhone(String phoneNumber, int personId) throws Throwable{
+		StringBuilder jsonStr = new StringBuilder();
+		jsonStr.append("{\"phonenumber\": \"" + phoneNumber + "\",").
+				append("\"person_id\": " + personId + "}");
+		
+		@SuppressWarnings("unused")
+		HttpResponse<JsonNode> jsonResponse = Unirest.post("http://localhost:8080/persons/addphone")
+				  .header("Content-Type", "application/json")
+				  .header("Accept", "application/json")
+				  .body(jsonStr.toString())
+				  .asJson();
+	}
+	
+	@Then("^Jumlah nomor telepon milik person dengan id (\\d+) menjadi jumlah awal \\+ (\\d+)$")
+	public void thenAddPhone(String personId, int jmlPenambahanNoTelp) throws Throwable{
+		HttpResponse<JsonNode> jsonResponse = Unirest.get("http://localhost:8080/persons/"+personId)
+				.header("Accept", "application/json")
+				.asJson();
+		JSONObject objPerson  = jsonResponse.getBody().getObject();
+		JSONArray arrPhoneNumber = objPerson.getJSONArray("phones");
+		assertThat(arrPhoneNumber.length(),equalTo(jmlNoTelpAwal+jmlPenambahanNoTelp));
+	}
 }
